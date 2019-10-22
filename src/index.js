a757178acf700571277fa2af712d9391e078ec58
Sarabjot Singh <info@sujjon.in>
2019-10-22 12:14:09 +1100

    new test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import Reactdom from "react-dom";
-import App from "./components/app.js";
+import App from "./components/App.js";
 
 Reactdom.render(<App />, document.getElementById("app"));
